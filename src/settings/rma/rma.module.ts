350c37860ca2cadeb829569e88826bc1a3cff051
muzammil-shayk <m.muzammil47@protonmail.com>
2025-08-20 13:58:38 +0500

    auth protection for RMA and Tags submodules

--- src/settings/rma/rma.module.ts
+++ src/settings/rma/rma.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { RmaController } from './controllers/rma.controller';
 import { RmaService } from './services/rma.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthMiddleware } from 'src/common/middleware/auth.middleware';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RmaController],
   providers: [RmaService],
 })
-export class RmaModule {}
+export class RmaModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(RmaController);
+  }
+}
--- src/settings/tags/tags.module.ts
+++ src/settings/tags/tags.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { TagsService } from './services/tags.service';
 import { TagsController } from './controllers/tags.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthMiddleware } from 'src/common/middleware/auth.middleware';
 
 @Module({
   imports: [PrismaModule],
   controllers: [TagsController],
   providers: [TagsService],
 })
-export class TagsModule {}
+export class TagsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(TagsController);
+  }
+}
